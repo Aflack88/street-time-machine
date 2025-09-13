fb10bf553d808b32ba17a8ce2eece90ff66afc05
Aflack88 <142524602+Aflack88@users.noreply.github.com>
2025-09-13 17:27:41 -0500

    Create next.config.js

---
+++ frontend/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [], // if you serve images from external host, add domains
+  },
+}
+
+module.exports = nextConfig
